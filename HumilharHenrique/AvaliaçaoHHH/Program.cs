0a18eaa3562be9c87f0093ca9bd5b82f6afa1edb
Emanuel1305 <emanuelfafa1313@gmail.com>
2023-03-28 09:51:00 -0400

    não entre

---
+++ HumilharHenrique/AvaliaçaoHHH/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaliaçaoHHH
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t----Verificação Peniana----\n");
+            Console.Write("Informe o tamanho do seu Pênis em centímetros: ");
+            int tamanho = int.Parse(Console.ReadLine());
+
+            if (tamanho >= 5 && tamanho <= 10)
+            {
+                Console.WriteLine("\nQue cut cut");
+            }
+            else if (tamanho >= 11 && tamanho <= 15)
+            {
+                Console.WriteLine("\nPode melhorar, 'Passa um Libid Gel que é sucesso!'");
+            }
+            else if (tamanho >= 16 && tamanho <= 20)
+            {
+                Console.WriteLine("\nQue isso em!! Tá potente, o tamanho ideal para arrasar corações!");
+            }
+            else if (tamanho >= 21 && tamanho <= 1000)
+            {
+                Console.WriteLine("\nQuem é Kid Bengala perto de vc slk, o famoso estora últero");
+            }
+            else if (tamanho >= 0 && tamanho <= 4)
+            {
+                Console.WriteLine("\nkkkkkkkkkkkk VC É UMA VERGONHA PRO SEXO MASCULINO");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HumilharHenrique/HumilharHenrique/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumilharHenrique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um valor de 1 a 5: ");
+            string opcao = Console.ReadLine();
+
+            switch (opcao)
+            {
+                case "1":
+                    Console.WriteLine("A Mãe do Henrique é esbelta");
+                    break;
+                case "2":
+                    Console.WriteLine("A Mãe do Henrique é totosa");
+                    break;
+                case "3":
+                    Console.WriteLine("A Mãe do Henrique é gata");
+                    break;
+                case "4":
+                    Console.WriteLine("A Mãe do Henrique é deliciosa");
+                    break;
+                case "5":
+                    Console.WriteLine("A Mãe do Henrique é maravilhosa");
+                    break;
+                default:
+                    Console.WriteLine("Comi sua Mãe meu grande amigo Henrique");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
