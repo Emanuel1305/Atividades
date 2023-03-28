97f00bb1c20c0b76b0122f12be5714d58a3c2c19
Emanuel1305 <emanuelfafa1313@gmail.com>
2023-03-28 08:56:39 -0400

    alteração

---
+++ AumetoSalario/AumetoSalario/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AumetoSalario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t----Calcula Aumento Salarial----");
+            Console.Write("\n\t[1] Gerente\n\t[2] Engenheiro\n\t[3] Técnico");
+            Console.Write("\n\nInforme o Cargo: ");
+            int cargo = int.Parse(Console.ReadLine());
+
+            Console.Write("\nInforme o Salário: ");
+            double salario = double.Parse(Console.ReadLine());
+
+            double novosalario = 0;
+
+            switch (cargo)
+            {
+                case 1:
+                    novosalario = salario + (salario * 0.1);
+                    Console.WriteLine();
+                    break;
+                case 2:
+                    novosalario = salario + (salario * 0.2);
+                    break;
+                case 3:
+                    novosalario = salario + (salario * 0.3);
+                    break;
+                default:
+                    novosalario = salario + (salario * 0.4);
+                    break;
+            }
+
+            Console.WriteLine($"\nSalário antigo: {salario.ToString("c2")}" +
+                $"\nNovo Salário: {novosalario.ToString("c2")}" +
+                $"\nValor do Reajuste: {(novosalario - salario).ToString("c2")}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CauculaNota/CauculaNota/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CauculaNota
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t----Calcula nota do aluno----\n");
+            Console.Write("Nome do Aluno: ");
+            string nome = Console.ReadLine();
+            Console.Write("\nNota do 1° Bimestre: ");
+            double n1 = double.Parse(Console.ReadLine());
+            Console.Write("Nota do 2° Bimestre: ");
+            double n2 = double.Parse(Console.ReadLine());
+            Console.Write("Nota do 3° Bimestre: ");
+            double n3 = double.Parse(Console.ReadLine());
+            Console.Write("Nota do 4° Bimestre: ");
+            double n4 = double.Parse(Console.ReadLine());
+
+            double media = (n1 + n2 + n3 + n4)/4;
+            char conceito = ' ';
+
+            if (media >= 90)
+            {
+                conceito = 'A';
+            } else if (media >= 80)
+            {
+                conceito = 'B';
+            } else if (media >= 60)
+            {
+                conceito = 'C';
+            }
+            else if (media >= 40)
+            {
+                conceito = 'D';
+            }
+            else if (media >= 20)
+            {
+                conceito = 'E';
+            }
+            else
+            {
+                Console.Write("o aluno está reprovado");
+            }
+
+            switch (conceito)
+            {
+                case 'A':
+                    Console.WriteLine($"\nNome:\t\t{nome}\n" +
+                        $"Conceito:\t{conceito}\n" +
+                        $"Situação:\tAprovado");
+                    break;
+                case 'B':
+                    Console.WriteLine($"\nNome:\t\t{nome}\n" +
+                        $"Conceito:\t{conceito}\n" +
+                        $"Situação:\tAprovado");
+                    break;
+                case 'C':
+                    Console.WriteLine($"\nNome:\t\t{nome}\n" +
+                        $"Conceito:\t{conceito}\n" +
+                        $"Situação:\tAprovado");
+                    break;
+                case 'D':
+                    Console.WriteLine($"\nNome:\t\t{nome}\n" +
+                        $"Conceito:\t{conceito}\n" +
+                        $"Situação:\tRecuperação");
+                    break;
+                case 'E':
+                    Console.WriteLine($"\nNome:\t\t{nome}\n" +
+                        $"Conceito:\t{conceito}\n" +
+                        $"Situação:\tExame");
+                    break;
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
