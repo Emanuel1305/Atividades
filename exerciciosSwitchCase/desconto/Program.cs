5644f9f9318c389e2717234445f61123a6956825
Emanuel1305 <emanuelfafa1313@gmail.com>
2023-04-04 09:54:21 -0400

    alteração

---
+++ exerciciosSwitchCase/desconto/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desconto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t----Calcula desconto de acordo com o cliente----\n");
+            Console.Write("Informe o valor do produto que deseja comprar: ");
+            double vp = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nCliente:" +
+                "\n\t[1] Comum" +
+                "\n\t[2] Funcionario" +
+                "\n\t[3] Vip");
+            int opc = Convert.ToInt32(Console.ReadLine());
+
+            switch (opc)
+            {
+                case 1:
+                    Limpar();
+                    Console.Write($"O valor total do seu produto é de {vp.ToString("c2")}");
+                    break;
+                case 2:
+                    Limpar();
+                    double desc = vp * 0.10;
+                    Console.Write($"O valor total do seu produto é de {(vp - desc).ToString("c2")}");
+                    break;
+                case 3:
+                    Limpar();
+                    double des = vp * 0.05;
+                    Console.Write($"O valor total do seu produto é de {(vp - des).ToString("c2")}");
+                    break;
+            }
+            Console.ReadKey();
+        }
+        private static void Limpar()
+        {
+            Console.Clear();
+            Console.WriteLine("\t\t----Calcula desconto de acordo com o cliente----\n");
+        }
+    }
+}
---
+++ exerciciosSwitchCase/exerciciosSwitchCase/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exerciciosSwitchCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t----Lê dois números e execulta operações com esses valores----\n");
+            double v1, v2;
+            Console.Write("Informe o primeiro número: ");
+            v1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Informe o segundo número: ");
+            v2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nInforme qual operação deseja realizar:" +
+                "\n\t[1] Adição" +
+                "\n\t[2] Subtração" +
+                "\n\t[3] Mutiplicação" +
+                "\n\t[4] Divisão");
+            int opc = Convert.ToInt32(Console.ReadLine());
+
+            switch (opc)
+            {
+                case 1:
+                    Limpar();
+                    Console.Write($"{v1} + {v2} = {v1 + v2}");
+                    break;
+                case 2:
+                    Limpar();
+                    Console.Write($"{v1} - {v2} = {v1 - v2}");
+                    break;
+                case 3:
+                    Limpar();
+                    Console.Write($"{v1} x {v2} = {v1 * v2}");
+                    break;
+                case 4:
+                    Limpar();
+                    Console.Write($"{v1} / {v2} = {v1 / v2}");
+                    break;
+                default:
+                    Limpar();
+                    Console.Write("O valor informado não é valido.");
+                    break;
+            }
+            Console.ReadLine();
+        }
+
+        private static void Limpar()
+        {
+            Console.Clear();
+            Console.WriteLine("\t\t----Lê dois números e execulta operações com esses valores----\n");
+        }
+
+    }
+}
