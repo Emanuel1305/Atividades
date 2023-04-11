2b66022780e4c05600bd9e055d90704a3fa18627
Emanuel1305 <emanuelfafa1313@gmail.com>
2023-04-11 10:15:01 -0400

    Aula1104

---
+++ TabuadaComWhile/TabuadaComWhile/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabuadaComWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int op = 1;
+            while (op == 1)
+            {
+                Console.WriteLine("\t\t----Tabuada com While----\n");
+                Console.Write("Informe um número: ");
+                int num = int.Parse(Console.ReadLine());
+                Console.Write("\n");
+
+                int cont = 0;
+
+                while (cont <= 10)
+                {
+                    Console.WriteLine($"{num}x{cont}={num * cont}");
+                    cont++;
+                }
+                Console.Write("\nDeseja realizar outra operação? \n[0]Não\n[1]Sim\n");
+                Console.Write("Escolha uma opção: ");
+                op = int.Parse(Console.ReadLine());
+                Console.Clear();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TarefasSlideWhile/Atividade_01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t---Calcular a Média Geral de uma Turma de alunos---\n");
+            Console.Write("Qual o número de alunos que a turma possui: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int cont = 1;
+            while (cont<=num)
+            {
+                Console.Write($"Informe a nota do {cont}° Aluno: ");
+                Console.Write("\n1° Bimestre: ");
+                double m1 = double.Parse(Console.ReadLine());
+                Console.Write("\n2° Bimestre: ");
+                double m2 = double.Parse(Console.ReadLine());
+                Console.Write("\n3° Bimestre: ");
+                double m3 = double.Parse(Console.ReadLine());
+                Console.Write("\n4° Bimestre: ");
+                double m4 = double.Parse(Console.ReadLine());
+            }
+
+        }
+    }
+}
