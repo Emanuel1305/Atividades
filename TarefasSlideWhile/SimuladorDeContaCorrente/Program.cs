9cf4897a05c86284609b2a5bec790ec9c3e00905
Emanuel1305 <emanuelfafa1313@gmail.com>
2023-04-13 10:13:32 -0400

    tarefa inacabada

---
+++ TarefasSlideWhile/SimuladorDeContaCorrente/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimuladorDeContaCorrente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double saldo = 0;
+            int opc = 0;
+            while (opc != 4)
+            {
+                opc = int.Parse(Console.ReadLine());
+                if (opc == 1)
+                {
+                    Console.Write($"Sua opção: [{opc}] Depositar" +
+                        $"\nQual valor deseja depositar? ");
+                    saldo = saldo + double.Parse(Console.ReadLine());
+
+                }
+                else if (opc == 2)
+                {
+                    Console.Write($"Sua opção: [{opc}] Sacar" +
+                        $"\nQual valor deseja sacar? ");
+                    saldo = saldo - double.Parse(Console.ReadLine());
+                }
+                else if (opc == 3)
+                {
+                    Console.Write($"Sua opção: [{opc}] Exibir Saldo" +
+                        $"\nSaldo: {saldo}");
+                }
+                else
+                {
+                    Console.WriteLine($"Sua opção: [{opc}] Sair");
+                    Console.WriteLine("Opção inválida");
+                }
+                opc = 0;
+                Console.Clear();
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
