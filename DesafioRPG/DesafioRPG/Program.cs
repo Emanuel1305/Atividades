f7734c39d9c57b5fea21f1a709ae68f794e031d1
Emanuel1305 <emanuelfafa1313@gmail.com>
2023-04-25 09:41:13 -0400

    alterações

---
+++ DesafioRPG/DesafioRPG/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioRPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inicializando a integridade do herói e do monstro
+            int heroIntegrity = 10;
+            int monsterIntegrity = 10;
+
+            // Loop principal do jogo
+            while (heroIntegrity > 0 && monsterIntegrity > 0)
+            {
+                // Ataque do herói
+                Console.WriteLine("\nHerói Ataca!");
+                int heroAttack = new Random().Next(1, 11); // Gera um valor aleatório entre 1 e 10
+                monsterIntegrity -= heroAttack;
+                Console.WriteLine($"O monstro foi danificado e perdeu {heroAttack} saúde e agora tem {monsterIntegrity} saúde.");
+
+                // Verifica se o monstro ainda está vivo antes de atacar
+                if (monsterIntegrity > 0)
+                {
+                    // Ataque do monstro
+                    Console.WriteLine("\nMontro Ataca!");
+                    int monsterAttack = new Random().Next(1, 11); // Gera um valor aleatório entre 1 e 10
+                    heroIntegrity -= monsterAttack;
+                    Console.WriteLine($"O herói foi danificado e perdeu {monsterAttack} saúde e agora tem {heroIntegrity} saúde.");
+                }
+            }
+
+            // Verifica quem venceu o jogo
+            if (heroIntegrity <= 0)
+            {
+                Console.WriteLine("\nO monstro venceu!");
+            }
+            else
+            {
+                Console.WriteLine("\nO herói venceu!");
+            }
+
+            // Aguarda o usuário pressionar alguma tecla antes de fechar a aplicação
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ProgramaCompras/ProgramaCompras/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramaCompras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t---Ajuda nas compras---\n");
+            Console.Write("Informe o valor que tem disponivel para compras: ");
+            double saldo = Double.Parse(Console.ReadLine());
+            double compra = 0, somaCompras = 0;
+            int contCompras = 0;
+            bool podeComprar = true;
+            do
+            {
+                Console.Write("\nInforme o valor da compra: ");
+                compra = Double.Parse(Console.ReadLine());
+                if (compra > 0)
+                {
+                    if (compra <= saldo)
+                    {
+                        saldo = saldo - compra;
+                        somaCompras = somaCompras + compra;
+                        contCompras++;
+                        Console.WriteLine("\nPode comprar\nSaldo disponivel: {0:c2}", saldo);
+                    }
+                    else
+                    {
+                        podeComprar = false;
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("\nCorrija o valor da compra!");
+                }
+
+            } while (podeComprar == true);
+            Console.Clear();
+            Console.WriteLine("\t\t---Resultado das Compras---");
+            Console.WriteLine("\nJá não pode comprar!\nSaldo Insuficiente!");
+            Console.WriteLine($"\nNúmero de compras: {contCompras}" +
+                $"\nTotal Gasto: {somaCompras:c2}" +
+                $"\nSaldo Restante: {saldo:c2}");
+            Console.ReadKey();
+        }
+    }
+}
