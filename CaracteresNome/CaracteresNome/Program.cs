863d8f9e1474e6335635b6dcf503dbb98cd934c9
Emanuel1305 <emanuelfafa1313@gmail.com>
2023-05-04 09:13:53 -0400

    caracteres de um nome

---
+++ CaracteresNome/CaracteresNome/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaracteresNome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t---Imprime o nome de acordo com a sua quantidade de caracteres---\n" +
+                "=======================================================================================");
+
+            Console.Write("Informe o nome: ");
+            string nome = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("\t\t---Imprime o nome de acordo com a sua quantidade de caracteres---\n" +
+                "=======================================================================================\n");
+
+            int carac = nome.Length;
+
+            Console.WriteLine($"{nome} possúi {carac} caracteres!\n");
+
+            for (int i = 1; i <= carac; i++)
+            {
+                Console.WriteLine($"{i} - {nome}");
+            }
+
+            Console.WriteLine("\n=======================================================================================");
+
+            Console.ReadKey();
+        }
+    }
+}
