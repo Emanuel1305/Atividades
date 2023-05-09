b906385a1c622baaf9049b427d89e9f62f01dcb6
Emanuel1305 <emanuelfafa1313@gmail.com>
2023-05-09 10:15:10 -0400

    j

---
+++ DesafioGraoDeArroz/DesafioGraoDeArroz/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioGraoDeArroz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal a = 1;
+            decimal b = 0;
+            decimal c = 0;
+            for (int i = 1; i <= 64; i++)
+            {
+                b = a * 2;
+                c = b * 2;
+                Console.WriteLine($"O {i} quadrado receberá: "+a.ToString("n2"));
+                a = b;
+                b = c;
+            }
+            Console.ReadKey();
+        }
+    }
+}
